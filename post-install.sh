f2981535a6ca2b33b2d889bde4a7882d2ec580ee
Wilson Holmes <wilsonjholmes@gmail.com>
2020-05-29 10:55:24 -0400

    Changed display manager to lightdm. Changed username and localhost to be mine. Added some cool ascii art.

--- post-install.sh
+++ post-install.sh
@@ -4,10 +4,29 @@
 # Origiinally forked from krushndayshmookh.github.io/krushn-arch.
 
 
-echo "Auto Archer"
+echo "
+
+                _                            _      
+     /\        | |            /\            | |     
+    /  \  _   _| |_ ___      /  \   _ __ ___| |__   
+   / /\ \| | | | __/ _ \    / /\ \ | '__/ __| '_ \  
+  / ____ \ |_| | || (_) |  / ____ \| | | (__| | | | 
+ /_/    \_\__,_|\__\___/  /_/    \_\_|  \___|_| |_| 
+                                                    
+                                                    
+  _____          _     _____           _        _ _ 
+ |  __ \        | |   |_   _|         | |      | | |
+ | |__) |__  ___| |_    | |  _ __  ___| |_ __ _| | |
+ |  ___/ _ \/ __| __|   | | | '_ \/ __| __/ _` | | |
+ | |  | (_) \__ \ |_   _| |_| | | \__ \ || (_| | | |
+ |_|   \___/|___/\__| |_____|_| |_|___/\__\__,_|_|_|
+                                                    
+                                                    
+
+"
 
 # Set date time
-ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+ln -sf /usr/share/zoneinfo/US/Michigan /etc/localtime
 hwclock --systohc
 
 # Set locale to en_US.UTF-8 UTF-8
@@ -16,8 +35,10 @@ locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
 # Set hostname
-echo "dayshmookh" >> /etc/hostname
-echo "127.0.1.1 dayshmookh.localdomain  dayshmookh" >> /etc/hosts
+echo "MCRN-Donnager" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1 localhost" >> /etc/hosts
+echo "127.0.1.1 MCRN-Donnager.localdomain  MCRN-Donnager" >> /etc/hosts
 
 # Generate initramfs
 mkinitcpio -P
@@ -30,13 +51,13 @@ grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=arch
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Create new user
-useradd -m -G wheel,power,iput,storage,uucp,network -s /usr/bin/zsh krushn
+useradd -m -G wheel,power,iput,storage,uucp,network -s /usr/bin/zsh wilson
 sed --in-place 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
-echo "Set password for new user krushn"
-passwd krushn
+echo "Set password for new user wilson"
+passwd wilson
 
 # Setup display manager
-systemctl enable sddm.service
+systemctl enable lightdm.service
 
 # Enable services
 systemctl enable NetworkManager.service
